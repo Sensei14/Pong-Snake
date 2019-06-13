6b81b6a086a5c547545ec63427cffe680a32ec2d
Sensei <karox1241@wp.pl>
2019-06-13 11:54:47 +0200

    Updates

---
+++ precache-manifest.014bf09f398f842211b7473a38ad0eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe51b7cd320061adb44916ba93ebeeee",
+    "url": "/Pong-Snake/index.html"
+  },
+  {
+    "revision": "667bd12234a38e7744ba",
+    "url": "/Pong-Snake/static/css/main.77c4555d.chunk.css"
+  },
+  {
+    "revision": "cdd9f4dc77a594a241ee",
+    "url": "/Pong-Snake/static/js/2.205b29d8.chunk.js"
+  },
+  {
+    "revision": "667bd12234a38e7744ba",
+    "url": "/Pong-Snake/static/js/main.2e36a6da.chunk.js"
+  },
+  {
+    "revision": "1fc8415edfb5d59415cc",
+    "url": "/Pong-Snake/static/js/runtime~main.8f9f2a1a.js"
+  }
+]);
